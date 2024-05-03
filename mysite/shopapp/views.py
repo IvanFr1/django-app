17f279ab5de13878e38cf576d5a05311454ffd13
ivanfrolov <ivan.frolov259@gmail.com>
2024-05-03 23:42:29 +0300

    adede practice 6.6

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect, reverse # type: ignore
 from django.contrib.auth.models import Group
 from .models import Product, Order
-from .forms import ProductForm
+from .forms import ProductForm, OrderForm
 
 
 def shop_index(request: HttpRequest):
@@ -54,3 +54,20 @@ def orders_list(request: HttpRequest):
         'orders': Order.objects.select_related("user").prefetch_related('products').all(),
     }
     return render(request, 'shopapp/orders-list.html', context=context)
+
+
+def create_order(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            url = reverse("shopapp:orders_list")
+            return redirect(url)
+    else:
+        form = OrderForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "shopapp/create-order.html", context=context)
